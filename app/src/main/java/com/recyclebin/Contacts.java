777e3d7e8960e038a027291be576709f3e78bc2b
Softwarica College Lab <softwarica@softwarica.com>
2019-04-19 14:29:02 +0545

    commit

--- app/src/main/java/com/recyclebin/Contacts.java
+++ app/src/main/java/com/recyclebin/Contacts.java
@@ -4,17 +4,22 @@ public class Contacts {
     private String name;
     private String phoneNo;
     private  int imageId;
+    private String address;
+    private String email;
 
-    //constructor
-    public Contacts(String name, String phoneNo,
-                    int imageId){
+    public Contacts(String name, String phoneNo, String address, String email ,int imageId) {
         this.name = name;
         this.phoneNo = phoneNo;
         this.imageId = imageId;
+        this.address = address;
+        this.email = email;
     }
-    public String getName(){return name;}
-    public void setName(String name)
-    {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
@@ -25,11 +30,29 @@ public class Contacts {
     public void setPhoneNo(String phoneNo) {
         this.phoneNo = phoneNo;
     }
-    public int getImageId(){
+
+    public int getImageId() {
         return imageId;
     }
 
     public void setImageId(int imageId) {
         this.imageId = imageId;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
