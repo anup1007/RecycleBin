6d34b7bf8407bed8a8347bba58661b908caceb8f
Softwarica College Lab <softwarica@softwarica.com>
2019-04-19 14:31:06 +0545

    commit

--- app/src/main/java/com/recyclebin/DetailsActivity.java
+++ app/src/main/java/com/recyclebin/DetailsActivity.java
@@ -8,7 +8,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DetailsActivity extends AppCompatActivity {
     CircleImageView cImg;
-    TextView tvFullName, tvPhoneNo;
+    TextView tvFullName, tvPhoneNo,tva,tve;
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -17,12 +17,18 @@ public class DetailsActivity extends AppCompatActivity {
        cImg = findViewById(R.id.imgProfile);
        tvFullName = findViewById(R.id.tvName);
        tvPhoneNo = findViewById(R.id.tvPhone);
+       tva = findViewById(R.id.tvaddress);
+       tve = findViewById(R.id.tvemail);
+
+        //Bundles are generally used for passing data between various Android activities
 
        Bundle bundle = getIntent().getExtras();
        if(bundle != null){
             cImg.setImageResource(bundle.getInt("image"));
             tvFullName.setText(bundle.getString("name"));
             tvPhoneNo.setText(bundle.getString("phone"));
+            tva.setText(bundle.getString("address"));
+            tve.setText(bundle.getString("email"));
        }
     }
 }
